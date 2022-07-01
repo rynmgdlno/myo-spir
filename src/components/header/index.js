b0736eb02d4414343f5550e473c3990b20c61649
rynmgdlno <rynmgdlno@gmail.com>
2022-07-01 11:28:32 -0700

    Continue work on header/menus

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,28 +1,72 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 
-import styles from './header.module.scss'
+import styles from "./header.module.scss";
+
+import { menuTree } from "../../generative/menuTree";
 
 const Header = props => {
-  const { menuTree } = props
-
+  // const { menuTree } = props
   return (
     <div className={styles.header}>
       <p>Header</p>
-      <Menu menuTree={menuTree}/>
+      <Menu menuTree={menuTree} />
     </div>
   );
 };
 
-export default Header
+export default Header;
 
-const Menu = (props) => {
-  const { menuTree } = props
-  // const staticMenu = 
+const Menu = props => {
+  const { menuTree } = props;
+  // const staticMenu =
   return (
     <nav>
-    {menuTree.filter((entry) => entry.fields.showInMenu).map((entry) => {
-      return <button key={entry.fields.id}>{entry.fields.title}</button>
-    })}
+      {menuTree.map(entry => {
+        if (entry.children) {
+          entry.children.map(child => {
+            {
+              /* return <MenuItem key={child.id} name={child.name}/>; */
+            }
+            return (
+              <p key={child.id}>
+                {child.name}
+              </p>
+            );
+          });
+        }
+      })}
     </nav>
-  )
-}
+  );
+};
+
+const MenuItem = ({ isMobile, title, children, menuToggle, setMenuToggle }) => {
+  const dropDownRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(
+    () => {
+      const pageClick = e => {
+        if (
+          dropDownRef.current !== null &&
+          !dropDownRef.current.contains(e.target)
+        ) {
+          setIsOpen(false);
+        }
+        if (isOpen) {
+          window.addEventListener("click", pageClick);
+        }
+      };
+      return () => {
+        window.removeEventListener("click", pageClick);
+      };
+    },
+    [isOpen, setIsOpen]
+  );
+
+  return (
+    <nav className={styles.menuItem}>
+      
+    </nav>
+  );
+};
--- src/generative/menuTree.js
+++ src/generative/menuTree.js
@@ -1,22 +1,88 @@
-const menuTree = [
-  {
-    id: 0,
-    name: 'Blog',
-    link: 'blog/'
-  },
-  {
-    id: 1,
-    name: 'Contact',
-    link: '/contact'
-  },
-  {
-    id: 2,
-    name: 'Refer a Patient',
-    link: '/referrals'
-  },
-  {
-    id: 3,
-    name: 'Book an Appointment',
-    link: '/appointments'
-  }
+export const menuTree = [
+	{
+		"name": "Tiffany Washburn",
+		"id": 0,
+		"link": null,
+		"children": [
+			{
+				"name": "About Me",
+				"id": 0,
+				"link": "/about-me",
+				"children": null
+			},
+			{
+				"name": "My Memberships",
+				"id": 1,
+				"link": "/my-memberships",
+				"children": null
+			}
+		]
+	},
+	{
+		"name": "Blog",
+		"id": 1,
+		"link": "/blog",
+		"children": null
+	},
+	{
+		"name": "What is Myofunctional Therapy?",
+		"id": 2,
+		"link": "/what-is-myofunctional-therapy",
+		"children": null
+	},
+	{
+		"name": "How Does it Work?",
+		"id": 3,
+		"link": "/how-does-it-work",
+		"children": null
+	},
+	{
+		"name": "Information",
+		"id": 4,
+		"link": null,
+		"children": [
+			{
+				"name": "Sleep Disordered Breathing",
+				"id": 0,
+				"link": "/sleep-disordered-breathing",
+				"children": null
+			},
+			{
+				"name": "TMJ and Facial Pain",
+				"id": 1,
+				"link": "/tmj-and-facial-pain",
+				"children": null
+			},
+			{
+				"name": "Speech and Tongue Thrust",
+				"id": 2,
+				"link": "/speech-and-tongue-thrust",
+				"children": null
+			},
+			{
+				"name": "Nasal Breathing",
+				"id": 3,
+				"link": "/nasal-breathing",
+				"children": null
+			}
+		]
+	},
+	{
+		"name": "Contact",
+		"id": 5,
+		"link": "/contact",
+		"children": null
+	},
+	{
+		"name": "Refer a Patient",
+		"id": 6,
+		"link": "/referrals",
+		"children": null
+	},
+	{
+		"name": "Book an Appointment",
+		"id": 7,
+		"link": "/book-an-appointment",
+		"children": null
+	}
 ]
