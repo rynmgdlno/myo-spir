34f801cfb23b00a7f916417dc63188764d4f177b
rynmgdlno <rynmgdlno@gmail.com>
2022-07-05 23:10:54 -0700

    Temp fix for server setup

---
+++ pages/[page].js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Page = () => {
+  return (
+    <main>
+      <Head>
+        <title>Page</title>
+        <meta name="Page" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+
+      </div>
+    </main>
+  );
+};
+
+export default Page
