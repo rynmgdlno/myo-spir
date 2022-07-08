27e5ca1687e8905da5b0d843e5d7c00a20b46037
Ryan Magdaleno <ryannmagdaleno@icloud.com>
2022-07-08 16:23:14 -0700

    Add header image to blog posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,10 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-import {
-  getOrderedCollection,
-  getSlugEntry
-} from "../../src/contentful";
+import { getOrderedCollection, getSlugEntry } from "../../src/contentful";
 import { renderOptions } from "../../src/contentful/richText";
 
 import styles from "./blog.module.scss";
@@ -36,21 +33,37 @@ export const getStaticProps = async context => {
 
 const Blog = props => {
   const data = props.data.items[0];
-  const { bodyText, date, description, title } = data.fields;
-  console.log(data)
+  const { bodyText, date, description, image, title } = data.fields;
+  const mainImage = {
+    title: image.fields.title,
+    alt: image.fields.description,
+    url: `https:${image.fields.file.url}`,
+    width: image.fields.file.details.image.width,
+    height: image.fields.file.details.image.height
+  };
   return (
     <div className={styles.page}>
       <Head>
-        <title>{title}</title>
+        <title>
+          {title}
+        </title>
         <meta name="Blog Post" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h3>{title}</h3>
+        <Image
+          src={mainImage.url}
+          height={mainImage.height}
+          width={mainImage.width}
+          alt={mainImage.alt}
+        />
+        <h3>
+          {title}
+        </h3>
         {data && documentToReactComponents(bodyText, renderOptions)}
       </main>
     </div>
   );
 };
 
-export default Blog
+export default Blog;
