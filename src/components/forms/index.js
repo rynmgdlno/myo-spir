bf9b0db01636f33a154c4d4c2cea744c747bff4f
Ryan Magdaleno <ryannmagdaleno@icloud.com>
2022-07-08 16:03:20 -0700

    Dynamic routes and fetching done for pages/blog posts

---
+++ src/components/forms/index.js
@@ -0,0 +1,24 @@
+export const AppointmentForm = () => {
+  return (
+    <div>
+      Appointment Form Goes Here
+    </div>
+  )
+}
+
+export const ContactForm = () => {
+  return (
+    <div>
+      Contact Form Goes Here
+    </div>
+  )
+}
+
+export const ReferralForm = () => {
+  return (
+    <div>
+      Referral Form Goes Here
+    </div>
+  )
+}
+
