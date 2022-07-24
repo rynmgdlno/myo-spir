a3ea24ce89ff88203944f6cabd15f835ec9495ad
Ryan Magdaleno <ryannmagdaleno@icloud.com>
2022-07-23 21:03:02 -0700

    Forms init

--- src/components/formInput/index.js
+++ src/components/formInput/index.js
@@ -1,13 +1,60 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import './formInput.scss'
-
-const FormInput = () => {
+export const Input = ({
+  name,
+  type,
+  placeholder,
+  className,
+  value,
+  error,
+  children,
+  label,
+  searchActive,
+  onChange,
+  ...props
+}) => {
+  
   return (
-    <div>
+    <>
+      {/* <label htmlFor={name}>{label}</label> */}
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        className={className}
+        onChange={onChange}
+        style={error && {border: 'solid 1px red'}}
+        {...props}
+      />
+      { error && <p>{ error }</p>}
+    </>
+  )
+}
 
-    </div>
-  );
-};
+export const Select = ({
+  className
+}) => {
+  return (
+    <select className={className}>
+      props.children
+    </select>
+  )
+}
 
-export default FormInput
+Input.defaultProps = {
+  type: "text",
+  className: ""
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  // placeholder: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['text', 'number', 'password', 'email', 'tel']),
+  className: PropTypes.string,
+  value: PropTypes.any,
+  // onChange: PropTypes.func.isRequired
+}
+
